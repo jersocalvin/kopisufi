a6cb71dbc29b6fdd83acef41ae25fd0cbf74c8ee
jersocalvin <jersocalvin@gmail.com>
2024-12-15 23:40:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Toggle class active
+const navbarNav = document.querySelector
+('.navbar-nav');
+// ketika humburger menu di klik
+document.querySelector('#hamburger-menu'). onclick = () => {
+navbarNav.classList.toggle('active');
+};
+
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector ('hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains (e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+
+    }
+});
+
+document.getElementById("#").addEventListener('cta', function(e) {
+    window.location.href = "https://wa.me/6289662944708";
+});
